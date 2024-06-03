7842a9533b6924b241799d8c3fdc8760afd5f288
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-06-03 20:38:51 -0300

    clase 13/intro JS

---
+++ clase 13/script.js
@@ -0,0 +1,21 @@
+console.log("Hola mundo!");
+
+// Variables
+
+var nombre = "Cristian";
+let apellido = "Vellio";
+let edad = 35;
+
+let tenerMascotas = true;
+
+console.log(nombre, apellido, edad, tenerMascotas);
+
+// Operaciones basicas
+
+let entrada = prompt("Ingrese su nombre");
+
+let salida = "Hola " + entrada;
+alert(salida);
+
+
+
